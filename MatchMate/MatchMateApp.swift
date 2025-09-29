34e7078c74ab5cb40ff1423a0085af8f3e20f012
Alok Ranjan <alok.ranjan@rksv.in>
2025-09-29 22:09:58 +0530

    Initial Commit

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Alok Ranjan on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatchMateTests/MatchMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateTests.swift
+//  MatchMateTests
+//
+//  Created by Alok Ranjan on 29/09/25.
+//
+
+import Testing
+@testable import MatchMate
+
+struct MatchMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
